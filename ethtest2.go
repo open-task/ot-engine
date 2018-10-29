940ca28ff82ca4f08e1db43525b8e9385ef8c0a3
Alex <work@xingyongtao.xyz>
2018-10-29 15:18:05 +0800

    update test2 for opentask v0.1.1

--- ethtest2.go
+++ ethtest2.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	openTask "./contracts"
 	"context"
 	"fmt"
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"math/big"
-
-	"github.com/ethereum/go-ethereum/ethclient"
-	openTask "./contracts"
+	"strings"
 )
 
 func main() {
@@ -22,25 +23,14 @@ func main() {
 
 	_ = client // we'll use this in the upcoming sections
 
-	address := common.HexToAddress("0xF562a7c51a158ae6E6170Ef7905af5d1cE43d24A") // Rinkeby
-	instance, err := openTask.NewOpenTask(address, client)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("contract is loaded")
-	_ = instance
-
-	owner, err := instance.Owner(nil);
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("contract owner is %s\n", owner.Hex()); // should be 0x1c635f4756ED1dD9Ed615dD0A0Ff10E3015cFa7b
+	contractAddress := common.HexToAddress("0xEB6af3deD23E2FA790C2D72B68e21dCC05c439d4") // Rinkeby
 
 	// filter logs
+	const FromBlock = 3244562;
+	const ToBlock = FromBlock + 1;
 	query := ethereum.FilterQuery{
-		FromBlock: big.NewInt(2394201),
-		ToBlock:   big.NewInt(2394201),
+		FromBlock: big.NewInt(FromBlock),
+		ToBlock:   big.NewInt(ToBlock),
 		Addresses: []common.Address{
 			contractAddress,
 		},
@@ -49,5 +39,31 @@ func main() {
 	logs, err := client.FilterLogs(context.Background(), query)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	contractAbi, err := abi.JSON(strings.NewReader(string(openTask.OpenTaskABI)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, vLog := range logs {
+		fmt.Printf("TxHash: %s\n", vLog.TxHash.Hex())
+
+		for j, vTopic := range vLog.Topics {
+			fmt.Printf("\t[Topic%d]: %s\n", j, vTopic.String())
+		}
+		fmt.Printf("Data: %v\n", vLog.Data)
+
+		event := struct {
+			missionId string
+			rewardInWei *big.Int
+		}{}
+		err := contractAbi.Unpack(&event, "Publish", vLog.Data)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("missionId: %s\n", event.missionId)
+		fmt.Printf("rewardInWei: %v\n", event.rewardInWei)
 	}
 }
