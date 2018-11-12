90bc2fb1f37b707b3fcc587448d77cb183923640
Alex <work@xingyongtao.xyz>
2018-11-12 17:00:25 +0800

    collect & process event log

--- collect/collect.go
+++ collect/collect.go
@@ -8,7 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum"
 	"math/big"
 	"context"
-	"github.com/xyths/ot-engine/decode"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func Collect(server string, address string, from int, to int, eventType string) {
@@ -35,24 +35,46 @@ func Collect(server string, address string, from int, to int, eventType string) 
 		log.Fatal(err)
 	}
 
+	publishSig := []byte("Publish(string,uint256)")
+	solveSig := []byte("Solve(string,string,string)")
+	acceptSig := []byte("Accept(string)")
+	rejectSig := []byte("Reject(string)")
+	confirmSig := []byte("Confirm(string,string)")
+
+	publishSigHash := crypto.Keccak256Hash(publishSig)
+	solveSigHash := crypto.Keccak256Hash(solveSig)
+	acceptSigHash := crypto.Keccak256Hash(acceptSig)
+	rejectSigHash := crypto.Keccak256Hash(rejectSig)
+	confirmSigHash := crypto.Keccak256Hash(confirmSig)
+
+	fmt.Printf(`publishSigHash: %s
+solveSigHash: %s
+acceptSigHash: %s
+rejectSigHash: %s
+confirmSigHash: %s
+`, publishSigHash.Hex(), solveSigHash.Hex(), acceptSigHash.Hex(), rejectSigHash.Hex(), confirmSigHash.Hex())
+
 	for i, vLog := range logs {
 		fmt.Printf("TxHash[%d]: %s\n", i, vLog.TxHash.Hex())
 
 		if len(vLog.Topics) >= 1 {
 			fmt.Printf("Sig(Topic0): %s\n", vLog.Topics[0].String())
 			switch vLog.Topics[0].String() {
-			case decode.PublishSig:
+			case publishSigHash.Hex():
 				fmt.Println("Publish")
-				Publish(vLog.Topics)
-			case decode.SolveSig:
+				Publish(vLog.Topics, vLog.Data)
+			case solveSigHash.Hex():
 				fmt.Println("Solve")
-				Solve(vLog.Topics)
-			case decode.AcceptSig:
+				Solve(vLog.Topics, vLog.Data)
+			case acceptSigHash.Hex():
 				fmt.Println("Accept")
-				Accept(vLog.Topics)
-			case decode.RejectSig:
+				Accept(vLog.Topics, vLog.Data)
+			case rejectSigHash.Hex():
 				fmt.Println("Reject")
-				Reject(vLog.Topics)
+				Reject(vLog.Topics, vLog.Data)
+			case confirmSigHash.Hex():
+				fmt.Println("Confirm")
+				Confirm(vLog.Topics, vLog.Data)
 			default:
 				//
 				fmt.Println("UNKNOWN Event Log")
@@ -62,6 +84,6 @@ func Collect(server string, address string, from int, to int, eventType string) 
 		for j, vTopic := range vLog.Topics {
 			fmt.Printf("\t[Topic%d]: %s\n", j, vTopic.String())
 		}
-		//fmt.Printf("Data: %v\n", vLog.Data)
+		fmt.Printf("Data: %v\n", vLog.Data)
 	}
 }
--- collect/process.go
+++ collect/process.go
@@ -3,24 +3,103 @@ package collect
 import (
 	"github.com/ethereum/go-ethereum/common"
 	"fmt"
+	"math/big"
+	"log"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"strings"
+	openTask "github.com/xyths/ot-engine/contracts"
 )
 
-func Publish(topics []common.Hash) {
-	fmt.Println("Publish")
+func Publish(topics []common.Hash, data []byte) {
+	event := struct {
+		MissionId string
+		RewardInWei *big.Int
+	}{}
 
+	contractAbi, err := abi.JSON(strings.NewReader(string(openTask.OpenTaskABI)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = contractAbi.Unpack(&event, "Publish", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("missionId: %s\n", event.MissionId)
+	fmt.Printf("rewardInWei: %v\n", event.RewardInWei.String())
 }
 
-func Solve(topics []common.Hash) {
+func Solve(topics []common.Hash, data []byte) {
 	fmt.Println("Solve")
+	event := struct {
+		SolutionId string
+		MissionId string
+		Data string
+	}{}
 
+	contractAbi, err := abi.JSON(strings.NewReader(string(openTask.OpenTaskABI)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = contractAbi.Unpack(&event, "Solve", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("solutionId: %s, missionId: %s, rewardInWei: %s\n", event.SolutionId,event.MissionId, event.Data)
 }
 
-func Accept(topics []common.Hash) {
+func Accept(topics []common.Hash, data []byte) {
 	fmt.Println("Accept")
+	event := struct {
+		SolutionId string
+	}{}
 
+	contractAbi, err := abi.JSON(strings.NewReader(string(openTask.OpenTaskABI)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = contractAbi.Unpack(&event, "Accept", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("solutionId: %s\n", event.SolutionId)
 }
 
-func Reject(topics []common.Hash) {
+func Reject(topics []common.Hash, data []byte) {
 	fmt.Println("Reject")
+	event := struct {
+		SolutionId string
+	}{}
 
+	contractAbi, err := abi.JSON(strings.NewReader(string(openTask.OpenTaskABI)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = contractAbi.Unpack(&event, "Reject", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("solutionId: %s\n", event.SolutionId)
+}
+
+func Confirm(topics []common.Hash, data []byte) {
+	fmt.Println("Confirm")
+	event := struct {
+		SolutionId string
+		ArbitrationId string
+	}{}
+
+	contractAbi, err := abi.JSON(strings.NewReader(string(openTask.OpenTaskABI)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = contractAbi.Unpack(&event, "Confirm", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("solutionId: %s, missionId: %s\n", event.SolutionId,event.ArbitrationId)
 }
