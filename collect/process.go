041b029d26a7a3530150b9c2c780630c7caeeba4
Alex <work@xingyongtao.xyz>
2018-11-19 16:40:37 +0800

    format

--- collect/process.go
+++ collect/process.go
@@ -1,18 +1,18 @@
 package collect
 
 import (
-	coreTypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/core/types"
 	"fmt"
 	"math/big"
 	"log"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"strings"
 	openTask "github.com/xyths/ot-engine/contracts"
-	otTypes "github.com/xyths/ot-engine/types"
+	. "github.com/xyths/ot-engine/types"
 )
 
 // 解析Publish日志。
-func Publish(vLog coreTypes.Log) (p otTypes.PublishEvent, err error) {
+func Publish(vLog types.Log) (p PublishEvent, err error) {
 	event := struct {
 		MissionId   string
 		RewardInWei *big.Int
@@ -38,7 +38,7 @@ func Publish(vLog coreTypes.Log) (p otTypes.PublishEvent, err error) {
 	return p, err
 }
 
-func Solve(vLog coreTypes.Log) (s otTypes.SolveEvent, err error) {
+func Solve(vLog types.Log) (s SolveEvent, err error) {
 	event := struct {
 		SolutionId string
 		MissionId  string
@@ -65,7 +65,7 @@ func Solve(vLog coreTypes.Log) (s otTypes.SolveEvent, err error) {
 	return s, err
 }
 
-func Accept(vLog coreTypes.Log) (a otTypes.AcceptEvent, err error) {
+func Accept(vLog types.Log) (a AcceptEvent, err error) {
 	event := struct {
 		SolutionId string
 	}{}
@@ -88,7 +88,7 @@ func Accept(vLog coreTypes.Log) (a otTypes.AcceptEvent, err error) {
 	return a, err
 }
 
-func Reject(vLog coreTypes.Log) (r otTypes.RejectEvent, err error) {
+func Reject(vLog types.Log) (r RejectEvent, err error) {
 	event := struct {
 		SolutionId string
 	}{}
@@ -111,7 +111,7 @@ func Reject(vLog coreTypes.Log) (r otTypes.RejectEvent, err error) {
 	return r, err
 }
 
-func Confirm(vLog coreTypes.Log) (c otTypes.ConfirmEvent, err error) {
+func Confirm(vLog types.Log) (c ConfirmEvent, err error) {
 	fmt.Println("Confirm")
 	event := struct {
 		SolutionId    string
